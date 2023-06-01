ada8c2e849d35ae07592cd7057bc8653e7321b2d
Prannaya <prannayagupta@gmail.com>
2023-06-01 13:16:10 +0800

    test font issues so one doesn't have to suffer from Times New Roman

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1670,7 +1670,7 @@ a {
     position: relative;
 }
 .card .card-footer .footer-box .box-wrapper .count {
-    font-family: 'consolas';
+    font-family: 'Courier New','consolas';
     color: #434343;
     font-size: 20px;
     font-weight: 600;
